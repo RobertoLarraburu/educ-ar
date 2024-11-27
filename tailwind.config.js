16aaf82755762643a5f10857b169015956fcdaa2
RobertoLarraburu <larryrober44@gmail.com>
2024-11-27 16:26:22 -0300

    Creacion del archivo de nosotros, mejoras a la navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,32 +15,3 @@ module.exports = {
   plugins: [],
 }
 }
-
-
-
-// remember to use module.exports instead of tailwind.config in production
-tailwind.config = {
-  // Note: config only includes the used styles & variables of your selection
-  content: ["./src/**/*.{html,vue,svelte,js,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {},
-      fontSize: {},
-      fontWeight: {},
-      lineHeight: {},
-      letterSpacing: {},
-      borderRadius: {},
-      colors: {
-        white: "#ffffff",
-        black: "#121212",
-      },
-      spacing: {},
-      width: {},
-      minWidth: {},
-      maxWidth: {},
-      height: {},
-      minHeight: {},
-      maxHeight: {},
-    },
-  },
-};
